6079295a716264bc7801e630125c12e9fd442695
Gusinet <felley.maxime74@gmail.com>
2022-10-12 17:25:25 +0200

    change to boob

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'weather App',
-        name: 'bob'
+        name: 'boob'
     })
 })
 
